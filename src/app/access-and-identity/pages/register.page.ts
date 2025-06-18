9419054b5dc6af0e835fa8ccf966a03569997b0b
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-06-17 19:45:00 -0500

    fix: register export class

--- src/app/access-and-identity/pages/register.page.ts
+++ src/app/access-and-identity/pages/register.page.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-import { RegisterComponent } from '../components/register/register.component';
+import { Component } from "@angular/core"
+import { RegisterComponent } from "../components/register/register.component"
 
 @Component({
-  selector: 'app-register-page',
+  selector: "app-register-page",
   standalone: true,
   imports: [RegisterComponent],
-  template: '<app-register></app-register>',
-  styles: []
+  template: `<app-register></app-register>`,
 })
 export class RegisterPage {}

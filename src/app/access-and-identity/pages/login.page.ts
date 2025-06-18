1dbb3ae3cf4d2119316ff033c37b42b9aff90a25
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-06-17 19:44:47 -0500

    fix: login export class

--- src/app/access-and-identity/pages/login.page.ts
+++ src/app/access-and-identity/pages/login.page.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-import { LoginComponent } from '../components/login/login.component';
+import { Component } from "@angular/core"
+import { LoginComponent } from "../components/login/login.component"
 
 @Component({
-  selector: 'app-login-page',
+  selector: "app-login-page",
   standalone: true,
   imports: [LoginComponent],
-  template: '<app-login></app-login>',
-  styles: []
+  template: `<app-login></app-login>`,
 })
 export class LoginPage {}

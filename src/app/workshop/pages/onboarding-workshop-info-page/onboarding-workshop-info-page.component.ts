ceaf1feed1b57cb20f8fbbffcf10a582c595a45e
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-07-09 03:22:56 -0500

    feat: ultra mega commit

--- src/app/workshop/pages/onboarding-workshop-info-page/onboarding-workshop-info-page.component.ts
+++ src/app/workshop/pages/onboarding-workshop-info-page/onboarding-workshop-info-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {WorkshopInfoComponent} from '../../components/workshop-info/workshop-info.component';
+import {ScheduleHoursComponent} from '../../components/schedule-hours/schedule-hours.component';
 
 @Component({
   selector: 'app-onboarding-workshop-info-page',
   imports: [
-    WorkshopInfoComponent
+    WorkshopInfoComponent,
+    ScheduleHoursComponent
   ],
   templateUrl: './onboarding-workshop-info-page.component.html',
   styleUrl: './onboarding-workshop-info-page.component.css'
